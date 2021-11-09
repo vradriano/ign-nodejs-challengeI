83040f91e011e4a5c8dde0c6e2883a3963d3363a
Vitor Adriano <vradriano@hotmail.com>
2021-11-08 23:52:22 -0300

    adicionando usuario

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const checkingUser = users.find(user => user.username === username);
+  
+  if(checkingUser){
+    return response.status(400).json({ error: "User already exists, please try again!"})
+  }
+
+  const newUsers = ({ 
+    id: uuidv4(),
+    name, 
+    username,
+    todos: []
+  })
+
+  users.push(newUsers)
+
+  return response.status(201).json(newUsers)
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
