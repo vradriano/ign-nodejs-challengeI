aad2dd84d6ab1a0a05e6ec6a1e6e3525f6a7ad61
Vitor Adriano <vradriano@hotmail.com>
2021-11-08 23:57:23 -0300

    adicionando post para criar novo todo

--- src/index.js
+++ src/index.js
@@ -53,7 +53,20 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body
+
+  const newTodos = ({ 
+    id: uuidv4(),
+    title,
+    done: false, 
+    deadline: new Date(deadline), 
+    created_at: new Date()
+  })
+
+  user.todos.push(newTodos)
+
+  return response.status(201).json(newTodos)
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
