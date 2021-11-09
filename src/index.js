293104877109d33433af7f9535e061630d39798b
Vitor Adriano <vradriano@hotmail.com>
2021-11-08 23:53:33 -0300

    adicionando middleware para verificar username

--- src/index.js
+++ src/index.js
@@ -11,7 +11,17 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({ error: "We have a problema Houston! Try again!!"})
+  }
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
